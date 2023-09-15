8202099bdc0792a36ac1a4e83c0dc8b964ec5aa0
Rennan <raljs@ecomp.poli.br>
2023-09-15 19:22:45 -0300

    Início de Canvas

--- JavaScript/estrelas.js
+++ JavaScript/estrelas.js
@@ -2,20 +2,32 @@ var y = Math.floor(Math.random() * 20) + "%";
 
 function atirar() {
 
-    var estrela = document.getElementById("estrela");
     var estrela1 = document.getElementById("estrela1");
     var estrela2 = document.getElementById("estrela2");
+    var estrela3 = document.getElementById("estrela3");
+    var estrela4 = document.getElementById("estrela4");
+    var estrela5 = document.getElementById("estrela5");
+    var estrela6 = document.getElementById("estrela6");
 
-    estrela.classList.toggle("voando");
     estrela1.classList.toggle("voando");
     estrela2.classList.toggle("voando");
+    estrela3.classList.toggle("voando");
+    estrela4.classList.toggle("voando");
+    estrela5.classList.toggle("voando");
+    estrela6.classList.toggle("voando");
 
-    estrela.style.paddingTop = y;
-    y = Math.floor(Math.random() * 15) + "%";
+    var variacao = 70
+
     estrela1.style.paddingTop = y;
-    y = Math.floor(Math.random() * 15) + "%";
+    y = Math.floor(Math.random() * variacao) + "%";
     estrela2.style.paddingTop = y;
+    y = Math.floor(Math.random() * variacao) + "%";
+    estrela3.style.paddingTop = y;
+    y = Math.floor(Math.random() * variacao) + "%";
+    estrela4.style.paddingTop = y;
+    y = Math.floor(Math.random() * variacao) + "%";
+    estrela5.style.paddingTop = y;
+    y = Math.floor(Math.random() * variacao) + "%";
+    estrela6.style.paddingTop = y;
 
 }
-
-//function mudar
